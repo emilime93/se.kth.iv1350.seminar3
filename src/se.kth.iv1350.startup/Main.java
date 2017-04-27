d2102275a385f6308832e673accc56a29dc6c6df
Emil <emil93@gmail.com>
2017-04-27 11:29:05 +0200

    Second comittment, still testing

--- src/se.kth.iv1350.startup/Main.java
+++ src/se.kth.iv1350.startup/Main.java
@@ -5,8 +5,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("start");
     }
-
-    public void abc() {
-        System.out.println();
-    }
 }
--- src/se/kth/iv1350/controller/Controller.java
+++
@@ -1,5 +0,0 @@
-package se.kth.iv1350.controller;
-
-public class Controller {
-
-}
